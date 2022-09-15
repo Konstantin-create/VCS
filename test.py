14dc59927824435c4494fdcd96ff16a9815ca63b
Konstantin-create <schahov.konst@gmail.com>
2022-09-15 21:40:39 +0300

    bug fix: to one command

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+"""
+In case with arg parser I should split some functions, like:
+    - In add function -c and -l flags will be going in other command, which will manage only this params,
+        and this isn't good...
+"""
+
+
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -22,10 +29,10 @@ create_parser.add_argument('-b', '--branch', dest='branch', help='default branch
 create_parser.set_defaults(func=init)
 
 add_parser = subparsers.add_parser('add', help='add data to db')
-add_parser.add_argument('filename', nargs='+', help='add(use . for add all files) file in a tracked list')
-add_parser.add_argument('-l', '--list', dest='mode', help='list of tracked files')
-add_parser.add_argument('-c', '--clean', dest='mode',
-                        help='clean tracked files list')
+add_parser.add_argument('-f', '--file', metavar='filename', nargs='+',
+                        help='add(use . for add all files) file in a tracked list', required=False)
+add_parser.add_argument('-l', '--list', nargs='?', default=True, help='print list of tracked files')
+add_parser.add_argument('-c', '--clean', nargs='?', default=True, help='clean list of tracked files')
 add_parser.set_defaults(func=add)
 
 if __name__ == '__main__':
